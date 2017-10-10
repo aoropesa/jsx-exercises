d5ab6ac7700594303bc2a95a4201c572afbcd476
aoropesa <aoropesa@fvi.edu>
2017-10-10 18:06:12 -0400

    first commit react

---
+++ src/index.js
@@ -0,0 +1,93 @@
+// 1) create a react app called jsx-exercises
+// 2) delete src/App.* src/index.css src/logo.svg (del src/App.*)
+// 3) In the index.js file, create a basic component similar to our HelloWorld component, call it whatever you like, and render some text out to the DOM.
+// 4) Create a component that renders this JSX.
+//
+// <div className="book">
+//   <div className="title">
+//     The Title
+//   </div>
+//   <div className="author">
+//     The Author
+//   </div>
+//   <ul className="stats">
+//     <li className="rating">
+//       5 stars
+//     </li>
+//     <li className="isbn">
+//       12-345678-910
+//     </li>
+//   </ul>
+// </div>
+//
+// 5) Make a copy of the the component from exercise 4, and replace the JSX with calls to React.createElement(). The output should be indentical.
+//
+// 6) Return the appropiate JSX from the following component so that when username is undefined or null,
+//    it renders 'Not logged in'. When username is a string, render "Hello, username".
+//
+// function Greeting() {
+//   // Try all of these variations
+//   // var username = 'root';
+//   // var username = undefined;
+//   // var username = null;
+//   // var username = false;
+//   //
+//   // Fill in the rest:
+//   //
+//   // return(...)
+// }
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting(){
+  var username = 'Alexander';
+  return (
+    <div>
+    <span>{username ? 'Hello ' + username : 'Not logged in'}</span>
+    <br />
+    <Component />
+    <br />
+    <Greeting />
+    </div>
+  )
+}
+// function Component(){
+//   return (
+//     <div className='book'>
+//       <div className='title'>
+//         The Title
+//     </div>
+//     <div className='author'>
+//       The Author
+//     </div>
+//     <ul className='starts'>
+//       <li className='rating'>
+//        5 starts
+//        </li>
+//       <li className='isbn'>
+//        12-345678-910
+//        </li>
+//     </ul>
+//     </div>
+//   );
+// }
+
+function Component(){
+  return React.createElement(
+    'div',
+    {className:'book'},
+    React.createElement('div', {className:'title'}, 'The Title'),
+    React.createElement('div', {className:'author'}, 'The Author'),
+    React.createElement('ul', {className:'starts'},
+      React.createElement('li',{className:'rating'},'5 starts'),
+      React.createElement('li',{className:'isbn'},'12-345678-910')
+    )
+  );
+}
+
+
+ReactDOM.render(
+  <Greeting />,
+  document.querySelector('#root')
+);
